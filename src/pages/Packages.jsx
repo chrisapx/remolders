7d91e2e1a0f838d3a22d4748e115b79e0f54f9a3
chrisapx <mcaplexyc@gmail.com>
2025-04-01 20:40:05 +0300

    Add package images

--- src/pages/Packages.jsx
+++ src/pages/Packages.jsx
@@ -27,24 +27,28 @@ const Packages = () => {
       description: 'Design Hub Bugolobi',
       color: 'bg-gradient-to-b hover:bg-gradient-to-t from-blue-500 to-blue-300',
       icon: <MdHolidayVillage size={40} className="text-blue-700" />,
+      image: '/images/Holiday package pic.jpeg'
     },
     {
       name: 'Young Adults',
       description: 'DH but also customizable setting',
       color: 'bg-gradient-to-b hover:bg-gradient-to-t from-green-500 to-green-300',
       icon: <ImManWoman size={40} className="text-green-700" />, 
+      image: '/images/Young adults pic.jpeg'
     },
     {
       name: 'Organizational package',
       description: "Divine Care Ministries’ testimonial",
       color: 'bg-gradient-to-b hover:bg-gradient-to-t from-gray-500 to-gray-300',
       icon: <CgOrganisation size={40} className="text-gray-700" />, 
+      image: '/images/Organizational package pic.jpeg'
     },
     {
       name: 'Institutional arrangements',
       description: 'Within schools offering varying curricula',
       color: 'bg-gradient-to-b hover:bg-gradient-to-t from-purple-500 to-purple-300',
       icon: <BiSolidInstitution size={40} className="text-purple-700" />, 
+      image: '/images/Institutional arrangement pic.jpeg'
     },
   ];
 
@@ -57,18 +61,19 @@ const Packages = () => {
         <div className="flex flex-wrap justify-center items-start gap-8">
           {packages.map((pkg, index) => (
             <div key={index} className="flex flex-col items-center max-w-sm">
-              <div
-                className={`relative group ${pkg.color} text-white w-[300px] h-[220px] rounded-lg shadow-lg cursor-pointer overflow-hidden`}
+              <div key={index}
+                className="relative w-[300px] h-[300px] border border-gray-300 rounded-lg shadow-xl overflow-hidden"
               >
-                <div className="absolute inset-0 flex flex-col items-center justify-center text-center font-bold" onClick={() => handleSelectPackage(pkg.name)}>
-                  {pkg.icon}
-                  <h2 className="text-xl mt-2">{pkg.name}</h2>
+                <img
+                  src={pkg.image || "/svgs/logo.svg"}
+                  className="w-full h-full object-cover"
+                  alt={`${pkg.name}'s pic`}
+                />
+                <div className="absolute bottom-0 w-full bg-black bg-opacity-70 text-white p-3">
+                  <h2 className="text-lg font-semibold">{pkg.name}</h2>
+                  <p className="text-sm">{pkg.description}</p>
                 </div>
               </div>
-
-              <p className="mt-4 w-[280px] text-center text-sm text-gray-700 leading-relaxed">
-                {pkg.description}
-              </p>
 
               <div className="mt-8 mb-16">
                 <button onClick={() => handleSelectPackage(pkg.name)} className="px-8 py-3 bg-black bg-opacity-70 hover:bg-opacity-100 text-white rounded-lg font-semibold">
